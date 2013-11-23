135f017fd6ee23a80af749f0a982a373a4f72c6c
AiE-Architect <eric.wohltman@gmail.com>
2013-11-23 17:30:35 -0500

    jenkinsBuild.sh

---
+++ jenkinsBuild.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# jenkinsBuild.sh
+
+for file in $(find /var/lib/jenkins/jobs/Epoch/workspace ! -path "*/.git/*" ! -name ".git*"); do
+	curl -u AiE:aie12345 -T $file ftp://198.27.80.126:8821/198.27.80.126_2362/;
+done
