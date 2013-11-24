6a3627ecdb59e2a109abd2c29f588cef8e282e13
AiE-Architect <eric.wohltman@gmail.com>
2013-11-23 20:37:37 -0500

    JABT

--- jenkinsBuild.sh
+++ jenkinsBuild.sh
@@ -2,5 +2,5 @@
 # jenkinsBuild.sh
 
 for file in $(find /var/lib/jenkins/jobs/epoch-repo/workspace -mmin -10 ! -path "*/.git/*" ! -name ".git*"); do
-	curl -u AiE:aie12345 -T $file ftp://198.27.80.126:8821/198.27.80.126_2362/;
+	curl --ftp-create-dirs -u AiE:aie12345 -T $file ftp://198.27.80.126:8821/198.27.80.126_2362/;
 done
