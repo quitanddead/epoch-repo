69c2ee71c7f9e80c686615e06c5c53c54a19e1aa
AiE-Architect <eric.wohltman@gmail.com>
2013-11-23 20:59:46 -0500

    JABT

--- jenkinsBuild.sh
+++ jenkinsBuild.sh
@@ -2,15 +2,15 @@
 # jenkinsBuild.sh
 
 for file in $(find /var/lib/jenkins/jobs/epoch-repo/workspace/ -mmin -10 ! -path "*/.git/*" ! -name ".git*"); do
-	lowerDir=$(echo $file | sed 's,/var/lib/jenkins/jobs/epoch-repo/workspace/,,g');
-	curl --retry 10 --ftp-create-dirs -u AiE:aie12345 -T $file ftp://198.27.80.126:8821/198.27.80.126_2362/$lowerDir;
 
 	#if [ -d $file ]; then
 	#	lowerDir=$(echo $file | sed 's,/var/lib/jenkins/jobs/epoch-repo/workspace/,,g')
 	#	curl --ftp-create-dirs -u AiE:aie12345 -T $file ftp://198.27.80.126:8821/198.27.80.126_2362/$lowerDir;
 	#fi
 
-	#if [ -f $file ]; then
-	#fi
+	if [ -f $file ]; then
+		lowerDir=$(echo $file | sed 's,/var/lib/jenkins/jobs/epoch-repo/workspace/,,g');
+		curl --retry 10 --ftp-create-dirs -u AiE:aie12345 -T $file ftp://198.27.80.126:8821/198.27.80.126_2362/;
+	fi
 
 done
