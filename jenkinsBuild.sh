ff8cb227de02927317b1927fe2a83d2bade557ee
AiE-Architect <eric.wohltman@gmail.com>
2013-11-24 04:06:32 -0500

    Improved jenkinsBuild.sh

--- jenkinsBuild.sh
+++ jenkinsBuild.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 # jenkinsBuild.sh
 
-for file in $(find /var/lib/jenkins/jobs/epoch-repo/workspace/ -mmin -10 ! -path "*/.git/*" ! -name ".git*"); do
+# curlIt function - to be called recursively until success
+curlIt ()
+{
+	theFile=$1;
+	lowerDir=$(echo $theFile | sed 's,/var/lib/jenkins/jobs/epoch-repo/workspace/,,g');
 
-	#if [ -d $file ]; then
-	#	lowerDir=$(echo $file | sed 's,/var/lib/jenkins/jobs/epoch-repo/workspace/,,g')
-	#	curl --ftp-create-dirs -u AiE:aie12345 -T $file ftp://198.27.80.126:8821/198.27.80.126_2362/$lowerDir;
-	#fi
-
-	if [ -f $file ]; then
-		echo "Transferring $file";
-		lowerDir=$(echo $file | sed 's,/var/lib/jenkins/jobs/epoch-repo/workspace/,,g');
+	if [ $2 -ne 1 ]; then
+		# First attempt with this file
+		echo "Transferring $theFile";
 		echo "Lower directory structure: $lowerDir";
-		curl --retry 10 --ftp-create-dirs -u AiE:aie12345 -T $file ftp://198.27.80.126:8821/198.27.80.126_2362/$lowerDir;
+
+		curl --retry 10 --ftp-create-dirs -u AiE:aie12345 -T $theFile ftp://198.27.80.126:8821/198.27.80.126_2362/$lowerDir;
+
+		if [ $? -ne 0 ]; then
+			# Error detected on transferring $theFile, set flag 1 to show retry
+			curlIt $theFile 1;
+		fi
 	fi
 
+	if [ $2 -eq 1 ]; then
+		echo "Retrying $theFile";
+		curl --retry 10 --ftp-create-dirs -u AiE:aie12345 -T $theFile ftp://198.27.80.126:8821/198.27.80.126_2362/$lowerDir;
+
+		if [ $? -ne 0 ]; then
+			# Error detected on transferring $theFile, set flag 1 to show retry
+			curlIt $theFile 1;
+		fi
+	fi
+}
+
+# Main function
+for file in $(find /var/lib/jenkins/jobs/epoch-repo/workspace/ -type f -mmin -10 ! -path "*/.git/*" ! -name ".git*"); do
+	# For all files in the epoch-repo that are not part of git, call curlIt with the full path of the file
+	curlIt $file;		
 done
